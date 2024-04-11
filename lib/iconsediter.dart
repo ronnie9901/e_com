62b0eeafde0b67d535cd13cefdc39b2584e12715
Rohan Nayak <nayakrohan9901@gmail.com>
2024-04-11 14:01:27 +0530

    155

--- lib/iconsediter.dart
+++ lib/iconsediter.dart
@@ -1,33 +1,20 @@
+import 'package:e_com/colorschange.dart';
 import 'package:e_com/column.dart';
 import 'package:e_com/utils/color.dart';
 import 'package:flutter/Material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(Myapp());
-}
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+
+class IconEditor extends StatefulWidget {
+  const IconEditor({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
-    );
-  }
+  State<IconEditor> createState() => _IconEditorState();
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
+class _IconEditorState extends State<IconEditor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,12 +76,12 @@ class _HomepageState extends State<Homepage> {
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: List.generate(colorslist.length, (index) => InkWell(onTap: (){
-                    setState(() {
-                      color=colorslist[index];
-                    });
-                  },child: con(c1:colorslist[index]))),
-
+                  children: List.generate(colorslist.length, (index) =>
+                      InkWell(onTap: () {
+                        setState(() {
+                          color = colorslist[index];
+                        });
+                      }, child: con(c1: colorslist[index]))),
 
 
                 ),
@@ -138,7 +125,7 @@ class _HomepageState extends State<Homepage> {
                           color: Colors.black12,
                           borderRadius: BorderRadius.all(Radius.circular(10))
                       ),
-                      child: Icon(Icons.add,size: 40,color: Colors.blue,),
+                      child: Icon(Icons.add, size: 40, color: Colors.blue,),
                     ),
                     SizedBox(width: 10,),
                     Container(
@@ -151,7 +138,8 @@ class _HomepageState extends State<Homepage> {
                           color: Colors.black12,
                           borderRadius: BorderRadius.all(Radius.circular(10))
                       ),
-                      child: Icon(Icons.add,size: 40,color: Colors.blue,),
+                      child: Icon(Icons.radio_button_checked, size: 40,
+                        color: Colors.blue,),
                     ),
 
                     SizedBox(width: 10,),
@@ -163,7 +151,8 @@ class _HomepageState extends State<Homepage> {
                           color: Colors.black12,
                           borderRadius: BorderRadius.all(Radius.circular(10))
                       ),
-                      child: Icon(Icons.arrow_back_ios,size: 40,color: Colors.blue,),
+                      child: Icon(
+                        Icons.arrow_back_ios, size: 40, color: Colors.blue,),
                     ),
                     SizedBox(width: 10,),
                     Container(
@@ -174,7 +163,8 @@ class _HomepageState extends State<Homepage> {
                           color: Colors.black12,
                           borderRadius: BorderRadius.all(Radius.circular(10))
                       ),
-                      child: Icon(Icons.arrow_forward_ios,size: 40,color: Colors.blue,),
+                      child: Icon(
+                        Icons.arrow_forward_ios, size: 40, color: Colors.blue,),
                     ),
                     SizedBox(width: 10,),
                     Container(
@@ -185,7 +175,7 @@ class _HomepageState extends State<Homepage> {
                           color: Colors.black12,
                           borderRadius: BorderRadius.all(Radius.circular(10))
                       ),
-                      child: Icon(Icons.alarm,size: 40,color: Colors.blue,),
+                      child: Icon(Icons.alarm, size: 40, color: Colors.blue,),
                     ),
                     SizedBox(width: 10,),
                     Container(
@@ -199,7 +189,7 @@ class _HomepageState extends State<Homepage> {
                           borderRadius: BorderRadius.all(Radius.circular(50))
 
                       ),
-                      child: Icon(Icons.call,size: 40,color: Colors.blue,),
+                      child: Icon(Icons.call, size: 40, color: Colors.blue,),
                     ),
                     SizedBox(width: 10,),
                     Container(
@@ -212,7 +202,7 @@ class _HomepageState extends State<Homepage> {
                           color: Colors.black12,
                           borderRadius: BorderRadius.all(Radius.circular(10))
                       ),
-                      child: Icon(Icons.search,size: 40,color: Colors.blue,),
+                      child: Icon(Icons.search, size: 40, color: Colors.blue,),
                     ),
                   ],
                 ),
@@ -222,20 +212,8 @@ class _HomepageState extends State<Homepage> {
         ));
   }
 
-  Container con({required c1}) {
-    return Container(
-
-      height: 80,
-      width: 80,
-
-      decoration: BoxDecoration(
-
-          color: c1,
-          borderRadius: BorderRadius.all(Radius.circular(50))
-      ),
-    );
-  }
 }
+
 Color color=Colors.blue;
 
 List colorslist=[
@@ -244,8 +222,7 @@ List colorslist=[
   Colors.brown,
   Colors.grey,
   Colors.yellow,
-
-
+  
 ];
 List iconslist=[
   Icons.add,
@@ -256,3 +233,4 @@ List iconslist=[
   Icons.search,
 
 ];
+
